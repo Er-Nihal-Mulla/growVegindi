f051fbf48061435cc88d408c6d8a99283142712c
Nihal Mulla <nihalmulla9784@gmail.com>
2025-08-14 19:04:21 +0000

    Fix Firebase env vars for Vercel

--- src/services/firebase-admin.ts
+++ src/services/firebase-admin.ts
@@ -6,9 +6,8 @@ if (!admin.apps.length) {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Use `JSON.parse` to handle the escaped newlines in the private key.
         privateKey: process.env.FIREBASE_PRIVATE_KEY
-          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
           : undefined,
       }),
     });
